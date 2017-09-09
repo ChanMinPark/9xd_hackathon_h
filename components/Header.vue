8baadcfb668b2415f16e11259b4a54e53bcebf07
ChanMinPark <walkinpcm@gmail.com>
2017-09-09 16:14:05 +0900

    room 페이지 추가, header 추가

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header">
+        <div class="logo">
+            GAMGI
+        </div>
+        <div class="menu">
+            채팅방 나가기
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.header {
+    width: 80%;
+    height: 70px;
+    margin-left: auto;
+    margin-right: auto;
+
+    background-color:darkcyan
+}
+</style>
---
+++ pages/room.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="container">
+        <div class="room-left">
+            <div class="video-panel">
+
+            </div>
+            <div class="control-panel">
+                <button>버튼1</button>
+                <button>버튼2</button>
+                <button>버튼3</button>
+                <button>버튼4</button>
+            </div>
+        </div>
+
+        <div class="room-right">
+            <div class="msg-list">
+
+            </div>
+            <div class="input-panel">
+                <input type="text">
+                <button>입력</button>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.container {
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 0;
+    padding-bottom: 0;
+}
+.container:after {
+    content: '';
+    display: block;
+    clear: both;
+}
+
+.room-left {
+    float: left;
+    width: 60%;
+    height: 100vh;
+    /* padding: 20px 20px; */
+
+    background-color: #e5c0c0;
+}
+
+.room-right {
+    float: left;
+    width: 40%;
+    height: 100vh;
+
+    background-color: #d6ce97;
+}
+
+.video-panel,
+.control-panel {
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 40px;
+
+    background-color: #d6ce97;
+}
+
+.video-panel {
+    height: 50%;
+}
+
+.control-panel {
+    height: 20%;
+}
+
+.control-panel button {
+    width: 15%;
+    height: 40px;
+    margin: 10px 20px;
+}
+
+.msg-list,
+.input-panel {
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 40px;
+
+    background-color: #e5c0c0;
+}
+
+.msg-list {
+    height: 70%;
+}
+
+.input-panel {
+    height: 10%;
+}
+</style>
